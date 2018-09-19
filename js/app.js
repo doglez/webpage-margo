238f90ec9eba0f8f53d81d2e87a654f8ad23705c
dgonzalezsiemi <dgonzalez.siemi@outlook.com>
2018-09-19 03:49:05 -0600

    ok

--- js/app.js
+++ js/app.js
@@ -10,6 +10,17 @@ $( document ).ready(function(){
 
 	/*JQuery para activar seleccion */
 	$('select').material_select()
+
+	/*JQuery para activar fanpage Facebook */
+	$.ajaxSetup({ cache: true });
+	  $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
+	    FB.init({
+	      appId: '{your-app-id}',
+	      version: 'v2.7' // or v2.1, v2.2, v2.3, ...
+	    });     
+	    $('#loginbutton,#feedbutton').removeAttr('disabled');
+	    FB.getLoginStatus(updateStatusCallback);
+	  });
 })    
 setInterval(function(){
   $('.carousel.carousel-slider').carousel('next');
