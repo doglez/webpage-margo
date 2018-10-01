ff343471ee12044bf6325f5cca43a80709e88401
dgonzalezmargo <danilo.gonzalez@margo.hn>
2018-10-01 03:51:11 -0600

    ok

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 $( document ).ready(function(){
 	/*JQuery para activar el sideNav*/
 	$(".button-collapse").sideNav();
-	
+
 	/*JQuery para activar el carrusel*/
 	$('.carousel.carousel-slider').carousel({
 		fullWidth:true,
 		indicators:true
 	}).css("height", $(window).height());
-})    
+	
+})
 setInterval(function(){
   $('.carousel.carousel-slider').carousel('next');
 }, 4000);
@@ -17,3 +18,4 @@ $(document).ready(function(){
     $('.scrollspy').scrollSpy();
 });
 
+$(document).on("scr")
