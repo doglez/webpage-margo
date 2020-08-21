cbf8a9b8d0ed46f4ebe0c491d819e3632b921777
danilogonzalez-mcs <danilo.j.gonzalez@gmail.com>
2020-08-21 16:16:52 -0600

    Animacion con materialize fadeInImage

--- index.php
+++ index.php
@@ -23,17 +23,17 @@
       </section>
       <section class="section_achievements grey lighten-3">
         <div class="row container">
-          <div class="col m4 s12 section_achievements_hover">
+          <div id="section_achievements_text1" class="col m4 s12 section_achievements_text animate_materlialize">
             <i class="large material-icons">assignment_ind</i>
             <h4>+10</h4>
             <h5>Años de experiencia</h5>
           </div>
-          <div class="col m4 s12 section_achievements_hover">
+          <div id="section_achievements_text2" class="col m4 s12 section_achievements_text animate_materlialize">
             <i class="large material-icons">sentiment_very_satisfied</i>
             <h4>100%</h4>
             <h5>De satisfacción</h5>
           </div>
-          <div class="col m4 s12 section_achievements_hover">
+          <div id="section_achievements_text3" class="col m4 s12 section_achievements_text animate_materlialize">
             <i class="large material-icons">account_balance</i>
             <h4>1000k</h4>
             <h5>En ahorro fiscal</h5>
@@ -41,20 +41,20 @@
         </div>
       </section>
       <section class="section_comments white-text">
-        <div class="row container">
-          <div class="card col m6 s12 laptop_upload_sar"></div>
-          <div class="card col m6 s12 section_comments_text blue-grey darken-1">
+        <div class="row container animateOpacity">
+          <div class="card z-depth-4 col m6 s12 laptop_upload_sar"></div>
+          <div class="card z-depth-4 col m6 s12 section_comments_text blue-grey darken-1">
             <h5 class="center">No te preocupes por tu declaración de impuestos.</h5>
             <p class="justify">Nosotros nos aseguramos de que todos los registros cumplan con la ley y estén a prueba de auditorías y nos encargamos de subir toda la información pertinente el día de la declaración tributaria. Sin problemas para ti y así podrás tener  más tiempo para concentrarte en tu negocio.</p>
           </div>
         </div>
-        <div class="row container">
-          <div class="card col s12 hide-on-med-and-up money_plants"></div>
-          <div class="card col m6 s12 section_comments_text blue-grey darken-1">
+        <div class="row container animateOpacity">
+          <div class="card z-depth-4 col s12 hide-on-med-and-up money_plants"></div>
+          <div class="card z-depth-4 col m6 s12 section_comments_text blue-grey darken-1">
             <h5 class="center">Beneficios de trabajar con nosotros.</h5>
             <p class="justify">Utilizando nuestros servicios administrativos y contabilidad especializados que brindamos, puedes ahorrar dinero ya que contamos con software que automatiza la reportería y análisis de gastos, así obtendrás un resultado de alta calidad a tiempo y garantizado.</p>
           </div>
-          <div class="card col m6 hide-on-small-only money_plants"></div>
+          <div class="card z-depth-4 col m6 hide-on-small-only money_plants"></div>
         </div>
       </section>
     </main>
--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
+// Materialize Animated
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
 	$('.carousel.carousel-slider').carousel({
 		fullWidth:true,
 	}).css("height", $(window).height());
 
-   $('.scrollspy').scrollSpy();
+  var options = [ {selector: '#section_achievements_text1', offset: 250, callback: function(el) { Materialize.fadeInImage($(el)); } }, {selector: '#section_achievements_text2', offset: 300, callback: function(el) { Materialize.fadeInImage($(el)); } }, {selector: '#section_achievements_text3', offset: 350, callback: function(el) { Materialize.fadeInImage($(el)); } } ]; Materialize.scrollFire(options);
 })
 
 setInterval(function(){
   $('.carousel.carousel-slider').carousel('next');
 	$('.carousel').carousel('next');
 }, 4000);
+
+// JavaScript Animated
+var animate = document.querySelectorAll('.animateOpacity');
+function mostrarScroll() {
+  var scrollTop = document.documentElement.scrollTop;
+  for (var i = 0; i < animateOpacity.length; i++) {
+    var alturaAnimate = animateOpacity[i].offsetTop;
+    if (alturaAnimate < scrollTop) {
+      animate[i].style.opacity = 1;
+    }
+  }
+}
+
+window.addEventListener('scroll', mostrarScroll);
