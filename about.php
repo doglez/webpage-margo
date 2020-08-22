b83329ef5b198a5eab09c9262524a71a3099f8e2
danilogonzalez-mcs <danilo.j.gonzalez@gmail.com>
2020-08-22 00:20:41 -0600

    Edicion de la pagina about.php

--- about.php
+++ about.php
@@ -8,7 +8,39 @@
     <?php include('header.php'); ?>
 
     <main>
-      
+      <section class="section_about_us">
+        <div class="row container">
+          <div id="section_about_us_text1" class="card col s12 hide-on-med-and-up img_why_us"></div>
+          <div id="section_about_us_text2" class="col m6 s12 why_us text_spacing">
+            <h3>Que hacemos?</h3>
+            <p class="justify_text">Ante la demanda de las Instituciones Estatales y el Sistema Financiero, las pequeñas y Medianas empresas y Profesionales en General deben estar al día con su información Financiera, para ello MARGO S. de R.L. está a la disposición de apoyarles ofreciéndoles los servicios Contables, Tributarios, Administrativos.</p>
+            <p class="justify_text">Contamos con un Software amigable en el cual la empresas pueden tener la seguridad de que el procesamiento de su información está protegida y disponible siempre, obteniendo datos confiables y confidenciales.</p>
+            <p class="justify_text bold_text">El éxito de nuestros clientes es nuestra prioridad y siempre estamos entusiasmados y preocupados por ellos.</p>
+          </div>
+          <div class="col m1 hide-on-small-only"></div>
+          <div id="section_about_us_text3" class="card col m5 hide-on-small-only img_why_us"></div>
+        </div>
+      </section>
+      <section class="section_information">
+        <div class="parallax">
+          <div class="row container">
+            <div class="col s12">
+              <h3>Nuestras áreas clave de operacion</h3>
+              <ul>
+                <li>Soluciones de outsourcing contable para pequeñas y medianas empresas</li>
+                <li>Servicio contabiles, tareas rutinarias como ser: facturación, conciliaciones, provisiones, amortizaciones, pasivos, etc.</li>
+                <li>Asistencia con la preparación de estados financieros</li>
+                <li>Informes de presupuesto y previsiones</li>
+                <li>Proyecciones a mediano y largo plazo dirigidos a inversores claves basadas en suposiciones prudentes con comprensión de los negocios del cliente</li>
+                <li>Planificación empresarial y estratégica</li>
+                <li>Asistencia en el registro, cómputo y declaración de impuestos</li>
+                <li>Tareas de gestión con informes de costes personalizados</li>
+                <li>Combinaciones de negocios y/o reestructuraciones corporativas</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
 
     <!--Import footer.php-->
