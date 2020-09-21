2c20b3f228f24f0f768cb8df5d88b26972d0577d
danilogonzalez-mcs <danilo.j.gonzalez@gmail.com>
2020-09-21 16:02:19 -0600

    parallax y contabilidad.php

--- contabilidad.php
+++ contabilidad.php
@@ -8,7 +8,20 @@
     <?php include('header.php'); ?>
 
     <main>
-      
+      <section class="section_service_parallax">
+        <div class="parallax-container">
+          <div class="parallax"><img src="img/accounting.jpg"></div>
+        </div>
+      </section>
+      <section class="section_service_message">
+        <div class="row container">
+          <div class="col s12">
+            <h3>¿Por qué los informes contables y financieros son tan importantes para las empresas?</h3><br>
+            <p>Los informes contables y financieros ayudan a controlar la cantidad de dinero que entra y sale de tu empresa,  y así podrás determinar qué tan bien está funcionando el negocio.</p>
+            <p>Supervisa de cerca esto mediante el registro de transacciones, el análisis de patrones y el manejo de la nómina y los impuestos directamente. Por otro lado, los informes financieros que incluyen la ganancia y pérdida, el balance general, el flujo de caja, los cambios en el patrimonio y las notas a los estados financieros, son los informes esenciales para apoyarte en la toma de decisiones correctas, dirigiendo la estrategia comercial, progresando para alcanzar las metas organizacionales.</p>
+          </div>
+        </div>
+      </section>
     </main>
 
     <!--Import footer.php-->
