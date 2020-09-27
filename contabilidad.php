82febf3f4782bbc4831952f2bce09d0b1ad99c9c
danilogonzalez-mcs <danilo.j.gonzalez@gmail.com>
2020-09-26 22:08:57 -0600

    se agrego efecto Upslide

--- contabilidad.php
+++ contabilidad.php
@@ -13,13 +13,21 @@
           <div class="parallax"><img src="img/accounting.jpg"></div>
         </div>
       </section>
-      <section class="section_service_message">
+      <section class="section_service_message animateAbove">
         <div class="row container">
           <div class="col s12">
             <h3>¿Por qué los informes contables y financieros son tan importantes para las empresas?</h3><br>
             <p>Los informes contables y financieros ayudan a controlar la cantidad de dinero que entra y sale de tu empresa,  y así podrás determinar qué tan bien está funcionando el negocio.</p>
             <p>Supervisa de cerca esto mediante el registro de transacciones, el análisis de patrones y el manejo de la nómina y los impuestos directamente. Por otro lado, los informes financieros que incluyen la ganancia y pérdida, el balance general, el flujo de caja, los cambios en el patrimonio y las notas a los estados financieros, son los informes esenciales para apoyarte en la toma de decisiones correctas, dirigiendo la estrategia comercial, progresando para alcanzar las metas organizacionales.</p>
           </div>
+        </div>
+      </section>
+      <section class="servicios_lista">
+        <div class="row">
+          <div class="col m6 s12">
+            
+          </div>
+          <div class="col m6 s12"></div>
         </div>
       </section>
     </main>
