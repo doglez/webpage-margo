64a60421aa5789372367dcc0b47beb1aa73578dd
danilogonzalez-mcs <danilo.j.gonzalez@gmail.com>
2020-10-23 22:19:34 -0600

    Terminada contabilidad y Contacto

--- contacto.php
+++ contacto.php
@@ -8,7 +8,65 @@
     <?php include('header.php'); ?>
 
     <main>
-      
+      <section id="formulario" class="formulario">
+        <div class="container">
+          <div class="row">
+            <div class="col s12">
+              <div class="card blue-grey z-depth-5">
+                <div class="card-content white-text center">
+                  <span class="card-title">Contactanos</span>
+                  <p>Puedes contactarte con nosotros por diversos medios, estaremos complacidos de atenderte:</p>
+                </div>
+                <div class="card-action">
+                  <div class="row">
+                    <div class="card-action row">
+                      <form class="col s12 m6" role="form" method="post" action="sender.php">
+                        <div class="row">
+                          <div class="input-field col s12">
+                            <input id="fname" name="fname" type="text" class="validate" required= "required">
+                            <label class=" white-text">Nombre Completo</label>
+                          </div>
+                          <div class="input-field col s12">
+                            <input id="email" name="email" type="email" class="validate" required= "required">
+                            <label data-error="wrong" data-success="right" class=" white-text">Correo electrónico</label>
+                          </div>
+                          <div class="input-field col s12">
+                            <input id="subj" name="subj" type="text" class="validate" required= "required">
+                            <label class=" white-text">Asunto</label>
+                          </div>
+                          <div class="input-field col s12">
+                            <textarea id="mssg" name="mssg" class="materialize-textarea validate" data-length="120" required= "required"></textarea>
+                            <label class=" white-text">Mensaje</label>
+                          </div>
+                          <button class="btn waves-effect waves-light red lighten-1" type="submit" id="send">Enviar
+                            <i class="material-icons right">send</i>
+                          </button>
+                        </div>
+                      </form>
+                      <div id="result-message" role="alert"></div>
+                      <div class="col m1 hide-on-small-and-down"></div>
+                      <div class="col s12 m4">
+                        <div class="row">
+                          <ul>
+                            <li><p class="white-text contacto2"><strong> <i class="material-icons tiny">phone</i> Telefóno: </strong><a href="tel: +504 22815651">(504) 2281-5651</a></p></li>
+                            <li><p class="white-text contacto2"><strong> <i class="material-icons tiny">phone_android</i> Celular: </strong><a href="tel: +504 32522185">(504) 3252-2185</a></p></li>
+                            <li><p class="white-text contacto2"><strong> <i class="material-icons tiny">email</i> Correo: </strong><a href="mailto:info@margo.hn?subject=Solicitud de información">info@margo.hn</a></p></li>
+                            <li><p class="white-text contacto2">
+                              <a href="https://www.linkedin.com/company/margohn/"><i class="fa fa-linkedin-square" style="font-size:26px"></i></a>
+                              <a href="https://www.facebook.com/MARGOSdeRL/"><i class="fa fa-facebook-square" style="font-size:26px"></i></a>
+                              <a href="https://twitter.com/MARGOSdeRL"><i class="fa fa-twitter-square" style="font-size:26px"></i></a></p></li>
+                            <li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>  
+      </section>    
     </main>
 
     <!--Import footer.php-->
--- header.php
+++ header.php
@@ -1,18 +1,16 @@
 <header>
   <div class="navbar-fixed">
-    <nav class="red darken-1">
-      <ul id="dropdown1" class="dropdown-content">
-        <li><a href="contabilidad.php">Contables</a></li>
-        <li><a href="administracion.php">Administrativos</a></li>
-        <li><a href="tributarios.php">Tributarios</a></li>
-      </ul>
+    <nav class="red darken-1">      
       <div class="nav-wrapper container">
         <a href="index.php" class="brand-logo"><img class="logo_white" src="img/logo_white.png" alt="MARGO"> </a>
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
           <li><a href="index.php">Inicio</a></li>
           <li><a href="about.php">Nosotros</a></li>
-          <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Servicios<i class="material-icons right">arrow_drop_down</i></a></li>
+          <li><a href="contabilidad.php">Contabilidad</a></li>
+          <li><a href="administracion.php">Administracíon</a></li>
+          <li><a href="tributarios.php">Tributacíon</a></li>
+          <li><a href="#">Software Contable</a></li>
           <li><a href="contacto.php">Contacto</a></li>
         </ul>
       </div>
@@ -23,12 +21,10 @@
     <li><img src="img/logo_normal.png" alt="MARGO" class="logo_sidenav"></li>
     <li><a href="index.php">Inicio</a></li>
     <li><a href="about.php">Nosotros</a></li>
-    <li><a href="#!">Servicios</a></li>
-    <li class="divider"></li>
-    <li><a class="sidenav_sublinks" href="contabilidad.php">Contables</a></li>
-    <li><a class="sidenav_sublinks" href="administracion.php">Administrativos</a></li>
-    <li><a class="sidenav_sublinks" href="tributarios.php">Tributarios</a></li>
-    <li class="divider"></li>
+    <li><a href="contabilidad.php">Contabilidad</a></li>
+    <li><a href="administracion.php">Administracíon</a></li>
+    <li><a href="tributarios.php">Tributacíon</a></li>
+    <li><a href="#">Software Contable</a></li>
     <li><a href="contacto.php">Contacto</a></li>
   </ul>
 
