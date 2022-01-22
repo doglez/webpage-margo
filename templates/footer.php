6ec8c08ef04f0cfc210e3d100f4eaa043e9f1299
doglez <danilo.j.gonzalez@gmail.com>
2022-01-22 11:26:00 -0600

    organizacion de archivos

---
+++ templates/footer.php
@@ -0,0 +1,41 @@
+  <footer class="page-footer red darken-1">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">MARGO S. de R. L.</h5>
+          <p class="grey-text text-lighten-4">Minister Business Club, Blvd. Suyapa, Tegucigalpa, Honduras</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text">Redes Sociales</h5>
+          <ul>
+            <li><a target="_blank" class="grey-text text-lighten-3" href="https://www.facebook.com/MARGOSdeRL/"><i class="fa fa-facebook-official" style="font-size:16px"></i> Facebook</a></li>
+            <li><a target="_blank" class="grey-text text-lighten-3" href="https://www.linkedin.com/company/margohn/"><i class="fa fa-linkedin-square" style="font-size:16px"></i> Linkedin</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="row container">
+        <div class="col l9 m12 s12">
+          <a class="grey-text text-lighten-4" href="https://www.mcscor.com">© 2020 Copyright MCS</a>
+        </div>
+        <div class="col l3 m12 s12">
+          <a class="grey-text text-lighten-4" href="privacy_policy.php">Política de Privacidad</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Import JavaScript Plugins -->
+  <!--Import JQuery-->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+  <!--JavaScript Materialize optimized loading-->
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+
+  <!--Import own JS-->
+  <script src="js/app.min.js"></script>
+
+  </body>
+
+  </html>
---
+++ templates/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="Doglez" content="MARGO">
+  <meta name="keywords" content="php, css, xml, javascript">
+  <title>MARGO Contabilidad, Administración, Impuestos, Tegucigalpa, Honduras</title>
+
+  <!--Import own css-->
+  <link rel="stylesheet" href="css/master.min.css">
+
+  <!-- Import Font Awesome Brand Icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!--Import logo Favicon-->
+  <link rel="icon" type="image/png" href="img/favicon-16x16.png" sizes="16x16" />
+
+  <!--Import Materialize Plugins-->
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+
+  <header>
+    <div class="navbar-fixed">
+      <nav class="red darken-1">
+        <div class="nav-wrapper container">
+          <a href="index.php" class="brand-logo"><img class="logo_white" src="img/logo_white.png" alt="MARGO"> </a>
+          <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="about.php">Nosotros</a></li>
+            <li><a href="contabilidad.php">Contabilidad</a></li>
+            <li><a href="administracion.php">Administración</a></li>
+            <li><a href="tributarios.php">Tributación</a></li>
+            <!-- <li><a href="#">Software Contable</a></li> -->
+            <li><a href="contacto.php">Contacto</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+
+    <ul class="side-nav" id="mobile-demo">
+      <li><img src="img/logo_normal.png" alt="MARGO" class="logo_sidenav"></li>
+      <li><a href="index.php">Inicio</a></li>
+      <li><a href="about.php">Nosotros</a></li>
+      <li><a href="contabilidad.php">Contabilidad</a></li>
+      <li><a href="administracion.php">Administración</a></li>
+      <li><a href="tributarios.php">Tributación</a></li>
+      <!-- <li><a href="#">Software Contable</a></li> -->
+      <li><a href="contacto.php">Contacto</a></li>
+    </ul>
+
+  </header>
