f784450318d0e3ec661fa454d4312ad827abad70
doglez <danilo.j.gonzalez@gmail.com>
2022-01-22 14:12:54 -0600

    administracion

---
+++ pages/administracion.php
@@ -0,0 +1,43 @@
+<main>
+  <section class="section_service_parallax">
+    <div class="parallax-container">
+      <div class="parallax"><img src="assets/img/admon.jpg"></div>
+    </div>
+  </section>
+  <section class="section_service_message animateAbove">
+    <div class="row container">
+      <div class="col s12">
+        <h3>Administracíon y Operación de Empresas</h3><br>
+        <p>Años de experiencia, conocimientos profundos en el mercado local como en el internacional hacen que en MARGO seamos competentes para comprender los entornos macro y micro en los que opera una empresa.</p>
+        <p>Además de lo anterior, también nos adaptamos a su empresa analizando su operación, cultura, objetivos, desafíos y dinámica competitiva. Esto se  traduce en soluciones efectivas y estrategias a la medida.</p>
+        <p>A través de nuestro enfoque colaborativo, de emplear una combinación de habilidades analíticas, una sólida gestión de proyectos y soluciones prácticas, estamos dedicados a ofrecer el valor prometido y según sus expectativas, asegurando así resultados sostenibles para su negocio.</p>
+        <ul>
+          <li>En conclusión, nuestro objetivo es:</li>
+          <li><i class="material-icons">check</i>Eficientar su negocio con nuestra experiencia</li>
+          <li><i class="material-icons">check</i>Agilice sus operaciones con nuestra experiencia</li>
+          <li><i class="material-icons">check</i>Triunfa en tus iniciativas con nuestras soluciones</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <section class="servicios_lista cyan lighten-3">
+    <div class="row">
+      <div class="col m6 s12 animateLeft">
+        <div class="img_adminis2"></div>            
+      </div>
+      <div class="col m6 s12 animateRight">
+        <h3>Servicios</h3>
+        <ul>
+          <li><i class="material-icons">done_all</i>Incorporación de Sociedades</li>
+          <li><i class="material-icons">done_all</i>Apertura y renovación de Permiso de Operación </li>
+          <li><i class="material-icons">done_all</i>Integración al Régimen de Facturación</li>
+          <li><i class="material-icons">done_all</i>Reingenieria de procesos</li>
+          <li><i class="material-icons">done_all</i>Solicitud de Constancias Estatales</li>
+          <li><i class="material-icons">done_all</i>Inscripción en las Instituciones de ente Público y Privado</li>
+          <li><i class="material-icons">done_all</i>Tercerización de Servicios Recursos Humanos</li>
+          <li><i class="material-icons">done_all</i>Liquidación de compañías</li>
+        </ul>
+      </div>          
+    </div>
+  </section>
+</main>
